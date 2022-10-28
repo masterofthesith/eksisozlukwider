5897a25d22ba040ba48778a8ca834a0f0f777266
Darth Vader <50755018+masterofthesith@users.noreply.github.com>
2022-10-28 21:12:39 +0300

    Update eksi.user.js

--- eksi.user.js
+++ eksi.user.js
@@ -25,7 +25,6 @@ function GM_addStyle(css) {
 }
 (function() {
     'use strict';
-    var $ = window.jQuery() || {};
     GM_addStyle("@import url(https://use.typekit.net/jxr8xbn.css);");
     GM_addStyle(":root::-webkit-scrollbar {width: 16px}");
     GM_addStyle(":root::-webkit-scrollbar-track {background: 0 0;box-sizing: border-box}");
@@ -48,10 +47,10 @@ function GM_addStyle(css) {
     GM_addStyle("h2 { color: #85c345!important; }");
     GM_addStyle("#entry-item-list .content, #matter-index-item-list .content, #matter-content .content, #matter-answer-content .content, #pinned-entry .content, #matter-answer-index-item-list .content, #matter-answer-list .content { font-weight:300!important; }");
     $(document).on("ready",function() {
-    $(document).find("header #top-bar").css("width","100%!important");
-    $(document).find("#container, .container").css({"width":"100%!important","max-width":"100%!important","padding":"0 15px!important"});
-    $(document).find("aside#aside").remove();
-    $(document).find("section#content-body").css({"width":"100%!important","margin":"0","padding":"25px"});
-    $(document).find("#nav-extra-buttons").remove();
+      $(document).find("header #top-bar").css("width","100%!important");
+      $(document).find("#container, .container").css({"width":"100%!important","max-width":"100%!important","padding":"0 15px!important"});
+      $(document).find("aside#aside").remove();
+      $(document).find("section#content-body").css({"width":"100%!important","margin":"0","padding":"25px"});
+      $(document).find("#nav-extra-buttons").remove();
     });
 })();
